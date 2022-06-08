7c16e8fc88e0e283a967673152ecae2435726001
Ntobeko Mvubu <84347265+NtobekoMvubu@users.noreply.github.com>
2022-06-08 20:04:20 +0200

    Created ORM and added configuration settings

---
+++ QuizAPI/Model/Status.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuizAPI.Model
+{
+    public partial class Status
+    {
+        public Status()
+        {
+            Questions = new HashSet<Question>();
+        }
+
+        public int StatusId { get; set; }
+        public string StatusName { get; set; } = null!;
+
+        public virtual ICollection<Question> Questions { get; set; }
+    }
+}
--- QuizAPI/Program.cs
+++ QuizAPI/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using QuizAPI.Model;
 using static QuizAPI.Middleware.APIKeyMiddleware;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +10,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+var connectionString = builder.Configuration.GetConnectionString("QuizDB");
+builder.Services.AddDbContext<TriviapiDBContext>(x => x.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
