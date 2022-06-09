afb81e9b88678669e13c5e501ef93de1b26fba59
aaronsh <aaronsh@bbd.co.za>
2022-06-09 21:54:09 +0200

    Added patch endpoint for udpating question. Also added util for getting id from relevant values.

--- QuizAPI/Controllers/QuizController.cs
+++ QuizAPI/Controllers/QuizController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using QuizAPI.Model;
+using QuizAPI.Utils;
 
 namespace QuizAPI.Controllers
 {
@@ -9,11 +10,66 @@ namespace QuizAPI.Controllers
     public class ValuesController : ControllerBase
     {
         TriviapiDBContext _context = new TriviapiDBContext();
+        ValueToIdUtil _valueToIdUtil = new ValueToIdUtil();
 
         [HttpGet(Name = "test")]
         public IActionResult testMe()
         {
             return new ObjectResult(_context.Questions.Find(1));
         }
-    }
+
+
+        [HttpPatch]
+        public Question updateQuestion(int id, [FromBody] Question questionPatches)
+        {
+            Question questionToChange = _context.Questions.Find(id);
+
+            if (!string.IsNullOrEmpty(questionPatches.Question1))
+            {
+                questionToChange.Question1 = questionPatches.Question1;
+            }
+
+            if (!string.IsNullOrEmpty(questionPatches.Answer))
+            {
+                questionToChange.Answer = questionPatches.Answer;
+            }
+
+            if (!string.IsNullOrEmpty(questionPatches.Difficulty.DifficultyName))
+            {
+                int difficultyForPatchId = _valueToIdUtil.getDifficulty(questionPatches.Difficulty.DifficultyName);
+                questionToChange.DifficultyId = questionToChange.Difficulty.DifficultyId = difficultyForPatchId;
+                questionToChange.Difficulty.DifficultyName = questionPatches.Difficulty.DifficultyName;
+                _context.Questions.Update(questionToChange);
+                _context.SaveChanges();
+            }
+
+            if (!string.IsNullOrEmpty(questionPatches.Category.CategoryName))
+            {
+                int categoryId = _valueToIdUtil.getCategory(questionPatches.Category.CategoryName);
+                questionToChange.CategoryId = questionToChange.Category.CategoryId = categoryId;
+                questionToChange.Category.CategoryName = questionPatches.Category.CategoryName;
+            }
+
+            if (!string.IsNullOrWhiteSpace(questionPatches.Status.StatusName))
+            {
+                int statusId = _valueToIdUtil.getStatus(questionPatches.Status.StatusName);
+                questionToChange.StatusId = questionToChange.Status.StatusId = statusId;
+                questionToChange.Status.StatusName = questionPatches.Status.StatusName;
+            }
+
+            try
+            {
+                _context.Questions.Update(questionToChange);
+                _context.SaveChanges();
+                return _context.Questions.Find(id);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Invalid values");
+            }
+            return null;
+
+
+        }
+        }
 }
---
+++ QuizAPI/Utils/ValueToIdUtil.cs
@@ -0,0 +1,35 @@
+﻿using QuizAPI.Model;
+using System.Linq;
+
+namespace QuizAPI.Utils
+{
+    public class ValueToIdUtil
+    {
+        TriviapiDBContext _context = new TriviapiDBContext();
+
+        public int getDifficulty(string difficultyName)
+        {
+            Difficulty difficultyToGet = (from d in _context.Difficulties
+                                          where d.DifficultyName == difficultyName
+                                          select d).ToList().FirstOrDefault();
+            return difficultyToGet.DifficultyId;
+        }
+
+        public int getCategory(string categoryName)
+        {
+            Category categoryToGet = (from c in _context.Categories
+                                      where c.CategoryName == categoryName
+                                      select c).ToList().FirstOrDefault();
+            return categoryToGet.CategoryId;
+        }
+
+        public int getStatus(string statusName)
+        {
+            Status statusToGet = (from s in _context.Statuses
+                                  where s.StatusName == statusName
+                                  select s).ToList().FirstOrDefault();
+
+            return statusToGet.StatusId;
+        }
+    }
+}
