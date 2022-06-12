b0b09517601b6c6c7d317ca4bffee69d8363e8d2
Carmen Janse van Rensburg <carmen@bbd.co.za>
2022-06-12 16:00:02 +0200

    added questiontag primary key

--- data/create.sql
+++ data/create.sql
@@ -56,6 +56,7 @@ CREATE TABLE QuestionTag (
      TagID INT NOT NULL,
 	 QuestionID INT NOT NULL,
 	 CONSTRAINT [FK_QuestionTag.TagID] FOREIGN KEY (TagID) REFERENCES Tag(TagID),
-	 CONSTRAINT [FK_QuestionTag.QuestionID] FOREIGN KEY (QuestionID) REFERENCES Question(QuestionID)
+	 CONSTRAINT [FK_QuestionTag.QuestionID] FOREIGN KEY (QuestionID) REFERENCES Question(QuestionID),
+     CONSTRAINT [PK_QuestionTag] PRIMARY KEY (TagID, QuestionID)
 );
 GO
