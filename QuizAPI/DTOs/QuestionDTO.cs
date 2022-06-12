2a343c47e5750db44aca5a6ab34cedd0f1a45d95
Carmen Janse van Rensburg <carmen@bbd.co.za>
2022-06-12 14:12:37 +0200

    question dto conversion function added

--- QuizAPI/DTOs/QuestionDTO.cs
+++ QuizAPI/DTOs/QuestionDTO.cs
@@ -1,4 +1,6 @@
-﻿namespace QuizAPI.DTOs
+﻿using QuizAPI.Model;
+
+namespace QuizAPI.DTOs
 {
 	public class QuestionDTO
 	{
@@ -9,11 +11,29 @@
 		private string? status;
 		private string[]? tags;
 
+		public QuestionDTO(String question, String answer, String status, String difficulty, String category, String[] tags)
+		{
+			this.question = question;
+			this.answer = answer;
+			this.status = status;
+			this.difficulty = difficulty;
+			this.category = category;
+			this.tags = tags; 
+		}
+
 		public string? Status { get => status; set => status = value; }
 		public string? Category { get => category; set => category = value; }
 		public string? Difficulty { get => difficulty; set => difficulty = value; }
 		public string? Answer { get => answer; set => answer = value; }
 		public string? Question { get => question; set => question = value; }
         public string[]? Tags { get => tags; set => tags = value; }
-    }
+
+		public static QuestionDTO AsDTO(Question question)
+		{
+			return new QuestionDTO(question.Question1, question.Answer, question.Status.StatusName, question.Difficulty.DifficultyName, question.Category.CategoryName, question.Tags );
+
+		}
+
+
+	}
 }
