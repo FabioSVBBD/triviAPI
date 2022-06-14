0ea8e46ebcca415698ab25a9940600bc43145d9c
aaronsh <aaronsh@bbd.co.za>
2022-06-14 10:34:16 +0200

    Made the utils singletons.

---
+++ QuizAPI/DTOs/InvalidResponseDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace QuizAPI.DTOs
+{
+    public class InvalidResponseDTO
+    {
+        private string message;
+        private List<string> possibleResponses;
+
+        public InvalidResponseDTO(string message, List<string> possibleResponses)
+        {
+            this.message = message;
+            this.possibleResponses = possibleResponses;
+        }
+
+        public string Message { get => message; set => message = value; }
+        public List<string> PossibleResponses { get => possibleResponses; set => possibleResponses = value; }
+    }
+}
---
+++ QuizAPI/Utils/ForeignKeyObjectsUtil.cs
@@ -0,0 +1,69 @@
+﻿using QuizAPI.Model;
+using System.Linq;
+using QuizAPI.DTOs;
+
+namespace QuizAPI.Utils
+{
+	public sealed class ForeignKeyObjectsUtil
+	{
+		private ForeignKeyObjectsUtil() { }
+
+		public static ForeignKeyObjectsUtil _foreignKeyObjectsUtilInstance;
+
+		public static ForeignKeyObjectsUtil getInstance()
+        {
+			if(_foreignKeyObjectsUtilInstance == null)
+            {
+				_foreignKeyObjectsUtilInstance = new ForeignKeyObjectsUtil();
+            }
+			return _foreignKeyObjectsUtilInstance;
+        }
+
+		TriviapiDBContext _context = new TriviapiDBContext();
+
+		public bool questionExists(int id)
+		{
+			return _context.Questions.Find(id) != null;
+		}
+
+		public Difficulty? getDifficultyObject(string difficultyName)
+		{
+			return (from d in _context.Difficulties
+					where d.DifficultyName.ToLower() == difficultyName.ToLower()
+					select d).ToList().FirstOrDefault();
+		}
+
+		public Category? getCategoryObject(string categoryName)
+		{
+			return (from c in _context.Categories
+					where c.CategoryName.ToLower() == categoryName.ToLower()
+					select c).ToList().FirstOrDefault();
+		}
+
+		public Status? getStatusByObject(string statusName)
+		{
+			return (from s in _context.Statuses
+					where s.StatusName.ToLower() == statusName.ToLower()
+					select s).ToList().FirstOrDefault();
+		}
+
+		public Tag? getTagObject(string tagName)
+        {
+			return (from t in _context.Tags
+					where t.TagName.ToLower() == tagName.ToLower()
+					select t).ToList().FirstOrDefault();
+		}
+
+		public List<string> getTagsForQuestion(int questionId)
+        {
+			return (from t in _context.Tags
+					join qt in _context.QuestionTags
+					on t.TagId equals qt.TagId
+					where qt.QuestionId == questionId
+					select new
+					{
+						tagName = t.TagName
+					}).ToList().Select(t => t.tagName).ToList();
+		}
+	}
+}
