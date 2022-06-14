d2f3291d4f355b94398ceb983c72ea8cc2a2339d
Carmen Janse van Rensburg <carmen@bbd.co.za>
2022-06-14 11:21:20 +0200

    added dto conversions

--- QuizAPI/DTOs/QuestionDTO.cs
+++ QuizAPI/DTOs/QuestionDTO.cs
@@ -8,20 +8,17 @@ namespace QuizAPI.DTOs
 		private string? answer;
 		private string? difficulty;
 		private string? category;
-		private string? status;
 		private string[]? tags;
 
-		public QuestionDTO(String question, String answer, String status, String difficulty, String category, String[]? tags)
+		public QuestionDTO(String question, String answer, String difficulty, String category, String[]? tags)
 		{
 			this.question = question;
 			this.answer = answer;
-			this.status = status;
 			this.difficulty = difficulty;
 			this.category = category;
 			this.tags = tags; 
 		}
 
-		public string? Status { get => status; set => status = value; }
 		public string? Category { get => category; set => category = value; }
 		public string? Difficulty { get => difficulty; set => difficulty = value; }
 		public string? Answer { get => answer; set => answer = value; }
@@ -34,7 +31,7 @@ namespace QuizAPI.DTOs
 				return null;
 			}
 
-			return new QuestionDTO(question.Question1, question.Answer, question.Status.StatusName, question.Difficulty.DifficultyName, question.Category.CategoryName, tags );
+			return new QuestionDTO(question.Question1, question.Answer, question.Difficulty.DifficultyName, question.Category.CategoryName, tags );
 		}
 	}
 }
